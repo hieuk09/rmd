449b6a222fa7e58198d11de3ccec072e3c933813
Hieu Nguyen <hieuk09@gmail.com>
2015-04-24 01:59:06 +0700

    [f] Add some others way to download

--- lib/rmd/downloader.rb
+++ lib/rmd/downloader.rb
@@ -13,30 +13,33 @@ module RMD
 
     def download
       puts file_name.green
+      #progress_bar = ProgressBar.create(
+        #starting_at: 0,
+        #total: nil,
+        #format: "%a %B %p%% %r KB/sec",
+        #rate_scale: lambda { |rate| rate / 1024 }
+      #)
 
-      progress_bar = ProgressBar.create(
-        starting_at: 0,
-        total: nil,
-        format: "%a %B %p%% %r KB/sec",
-        rate_scale: lambda { |rate| rate / 1024 }
-      )
+      #content_length_proc = Proc.new { |content_length|
+        #progress_bar.total = content_length
+      #}
 
-      content_length_proc = Proc.new { |content_length|
-        progress_bar.total = content_length
-      }
+      #progress_proc = Proc.new { |bytes_transferred|
+        #if progress_bar.total && progress_bar.total < bytes_transferred
+          #progress_bar.total = nil
+        #end
+        #progress_bar.progress = bytes_transferred
+      #}
 
-      progress_proc = Proc.new { |bytes_transferred|
-        if progress_bar.total && progress_bar.total < bytes_transferred
-          progress_bar.total = nil
-        end
-        progress_bar.progress = bytes_transferred
-      }
+      #open(link, "rb", content_length_proc: content_length_proc, progress_proc: progress_proc) do |page|
+        #File.open(file_path, "wb") do |file|
+          #file.write(page.read)
+        #end
+      #end
 
-      open(link, "rb", content_length_proc: content_length_proc, progress_proc: progress_proc) do |page|
-        File.open(file_path, "wb") do |file|
-          file.write(page.read)
-        end
-      end
+      system("aria2c #{link.inspect} -U 'Wget/1.8.1' -x4 -d #{File.dirname(file_path).inspect} -o #{File.basename(file_path).inspect}")
+      #system("wget -c #{link.inspect} -O #{file_path.inspect}")
+      #system("curl -A 'Wget/1.8.1' --retry 10 --retry-delay 5 --retry-max-time 4  -L #{link.inspect} -o #{file_path.inspect}")
     end
 
     def self.download(link, options = {})
